ebd035aa1fbad51f6f544a7ca78d8fa31040d0a6
patrik.hultgren <info@patrikhultgren.se>
2023-04-06 09:10:47 +0200

    Add placeholder

--- src/components/Weather/Forecast/index.tsx
+++ src/components/Weather/Forecast/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import Container from 'components/Container'
 import Day from './Day'
+import Placeholder from './Placeholder'
 
 interface IProps {
   weather: any
@@ -12,9 +13,16 @@ export default function Forecast({ weather, className }: IProps) {
 
   return (
     <Container className={classNames('mb-10', className)}>
-      {days?.map((day: any, dayIndex: number) => (
-        <Day day={day} dayIndex={dayIndex} key={day[0].time} />
-      ))}
+      {days
+        ? days.map((day: any, dayIndex: number) => (
+            <Day day={day} dayIndex={dayIndex} key={day[0].time} />
+          ))
+        : [0, 1, 2, 4, 5, 6, 7, 8, 9].map((placeholderIndex: number) => (
+            <Placeholder
+              key={placeholderIndex}
+              className="mt-6 first:mt-0 md:first:mt-4"
+            />
+          ))}
     </Container>
   )
 }
