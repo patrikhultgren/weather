b5f353ba70ed312216f5ab3fba76fe67bd6abbdd
patrik.hultgren <info@patrikhultgren.se>
2023-04-19 01:01:26 +0200

    Possible to hide error

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,7 +1,8 @@
+import { useCallback, useEffect, useState } from 'react'
 import classNames from 'classnames'
-
 import Container from 'components/Container'
 import ErrorIcon from 'components/Icon/Error'
+import Close from 'components/Icon/Close'
 import { IError } from 'utils/types'
 
 interface Props {
@@ -9,13 +10,28 @@ interface Props {
   className?: string
 }
 
-const Error = ({ error, className }: Props) => (
-  <div className={classNames('bg-red-100', 'p-4', className)}>
-    <Container className="flex items-center">
-      <ErrorIcon />
-      <span className="ml-2">{error.message}</span>
-    </Container>
-  </div>
-)
+const Error = ({ error, className }: Props) => {
+  const [show, setShow] = useState<boolean>(true)
+
+  const hideError = useCallback(() => {
+    setShow(false)
+  }, [])
+
+  useEffect(() => {
+    setShow(true)
+  }, [error])
+
+  return show ? (
+    <div className={classNames('bg-red-100', 'p-4', className)}>
+      <Container className="flex items-center">
+        <ErrorIcon />
+        <span className="mx-2">{error.message}</span>
+        <button type="button" className="ml-auto p-3" onClick={hideError}>
+          <Close title="Stäng felmeddelande" />
+        </button>
+      </Container>
+    </div>
+  ) : null
+}
 
 export default Error
--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -11,8 +11,8 @@ export default function Weather() {
 
   return (
     <>
-      {!weather.searchHandler.active && <Header weather={weather} />}
       <Loading loading={weather.loading} error={weather.error} />
+      {!weather.searchHandler.active && <Header weather={weather} />}
       {!weather.searchHandler.active && <Forecast weather={weather} />}
       {!weather.searchHandler.active && <Credit />}
       <SearchBar searchHandler={weather.searchHandler} />
