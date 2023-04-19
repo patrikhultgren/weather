bf8fa838a30f36aee26df1eedb147cc9c8cf3e07
patrik.hultgren <info@patrikhultgren.se>
2023-04-19 10:10:48 +0200

    Add default error message

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -35,7 +35,7 @@ const Error = ({ error, className }: Props) => {
     <div className={classNames('bg-red-100', 'px-4', 'py-2', className)}>
       <Container className="flex items-center">
         <ErrorIcon />
-        <span className="mx-2">{error.message}</span>
+        <span className="mx-2">{error.message || 'Någonting gick fel'}</span>
         <button type="button" className="ml-auto p-3" onClick={hideError}>
           <Close title="Stäng felmeddelande" size={18} />
         </button>
