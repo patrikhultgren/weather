31bd5e2e4dc7afa7e6ff27f5533132e762143d93
patrik.hultgren <info@patrikhultgren.se>
2023-04-19 07:27:34 +0200

    Change error styling

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -22,12 +22,12 @@ const Error = ({ error, className }: Props) => {
   }, [error])
 
   return show ? (
-    <div className={classNames('bg-red-100', 'p-4', className)}>
+    <div className={classNames('bg-red-100', 'px-4', 'py-2', className)}>
       <Container className="flex items-center">
         <ErrorIcon />
         <span className="mx-2">{error.message}</span>
         <button type="button" className="ml-auto p-3" onClick={hideError}>
-          <Close title="Stäng felmeddelande" />
+          <Close title="Stäng felmeddelande" size={18} />
         </button>
       </Container>
     </div>
--- src/components/Icon/Close/index.tsx
+++ src/components/Icon/Close/index.tsx
@@ -2,8 +2,11 @@ import { VscClose } from 'react-icons/vsc'
 
 interface IProps {
   title?: string
+  size?: number
 }
 
-const Close = ({ title }: IProps) => <VscClose title={title} size={28} />
+const Close = ({ title, size = 28 }: IProps) => (
+  <VscClose title={title} size={size} />
+)
 
 export default Close
