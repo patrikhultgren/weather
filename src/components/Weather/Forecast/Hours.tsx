3ec1f11d1261ff99999bafdc918f27c9887e0200
patrik.hultgren <info@patrikhultgren.se>
2023-04-21 01:43:06 +0200

    Add required tag

--- src/components/Weather/Forecast/Hours.tsx
+++ src/components/Weather/Forecast/Hours.tsx
@@ -7,12 +7,16 @@ interface IProps {
 }
 
 export default function Hours({ hours }: IProps) {
-  return hours.map((hour, index) => (
-    <tr
-      key={hour.time}
-      className={classNames({ 'bg-slate-50': index % 2 === 0 })}
-    >
-      <Hour hour={hour} />
-    </tr>
-  ))
+  return (
+    <>
+      {hours.map((hour, index) => (
+        <tr
+          key={hour.time}
+          className={classNames({ 'bg-slate-50': index % 2 === 0 })}
+        >
+          <Hour hour={hour} />
+        </tr>
+      ))}
+    </>
+  )
 }
