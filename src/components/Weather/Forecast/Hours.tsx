9eadeb7d0d47877d0d495d8762e7ec91ca0cf082
patrik.hultgren <info@patrikhultgren.se>
2023-04-21 01:38:27 +0200

    More typescript

--- src/components/Weather/Forecast/Hours.tsx
+++ src/components/Weather/Forecast/Hours.tsx
@@ -1,12 +1,13 @@
 import classNames from 'classnames'
+import { ITimeSerie } from 'utils/types'
 import Hour from './Hour'
 
 interface IProps {
-  hours: any
+  hours: Array<ITimeSerie>
 }
 
 export default function Hours({ hours }: IProps) {
-  return hours.map((hour: any, index: number) => (
+  return hours.map((hour, index) => (
     <tr
       key={hour.time}
       className={classNames({ 'bg-slate-50': index % 2 === 0 })}
