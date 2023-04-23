ae3e17b39db38ff01562ca275840e17d01e7c223
patrik.hultgren <info@patrikhultgren.se>
2023-04-24 00:59:05 +0200

    Add basic chart

--- src/components/Pages/Charts/Chart/index.tsx
+++ src/components/Pages/Charts/Chart/index.tsx
@@ -1,4 +1,6 @@
+import { useMemo } from 'react'
 import { IApp } from 'utils/types'
+import { format } from 'utils/date'
 import {
   LineChart,
   Line,
@@ -10,74 +12,48 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-  },
-]
-
 interface IProps {
   app: IApp
 }
 
 export default function Forecast({ app }: IProps) {
-  console.log('app', app)
+  const data = useMemo(() => {
+    let hours = []
+
+    if (app.days) {
+      let currentDay = 0
+
+      for (const day of app.days) {
+        for (const hour of day) {
+          hours.push({
+            airTemperature: hour.data.instant.details.air_temperature,
+            hour: format(hour.time, 'HH'),
+          })
+        }
+
+        if (currentDay == 2) {
+          break
+        }
+
+        currentDay++
+      }
+    }
+
+    return hours
+  }, [app])
+
   return (
-    <div className="ml-10 mr-20 my-10 mb-20">
-      <ResponsiveContainer width="100%" aspect={3} className="">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={
-            {
-              // top: 5,
-              // right: 30,
-              // left: 20,
-              // bottom: 5,
-            }
-          }
-        >
+    <div className="md:ml-10 md:mr-20 md:my-10 md:mb-20">
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart width={500} height={300} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="hour" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="airTemperature" stroke="#dc2626" />
         </LineChart>
       </ResponsiveContainer>
     </div>
   )
 }
-
-// ;<Line
-//   type="monotone"
-//   dataKey="pv"
-//   stroke="#8884d8"
-//   activeDot={{ r: 8 }}
-// />
