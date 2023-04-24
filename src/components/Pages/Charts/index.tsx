f14ec84bc46f3cb55a8ad79cf4dc6184f606c2ee
patrik.hultgren <info@patrikhultgren.se>
2023-04-24 12:00:56 +0200

    Maybe add key works

--- src/components/Pages/Charts/index.tsx
+++ src/components/Pages/Charts/index.tsx
@@ -23,7 +23,7 @@ export default function ChartsPage({ app }: IProps) {
     <main role="main">
       <Loading status={app.status} error={app.error} />
       <Header app={app} />
-      <NavBar app={app} activeMenuItem="charts" />
+      <NavBar app={app} activeMenuItem="charts" key="charts" />
       {appState === 'show-forecast' ? (
         <Charts app={app} />
       ) : (
