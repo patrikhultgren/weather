c1b19662026c98a063237a4b3fb9e0336376eb75
patrik.hultgren <info@patrikhultgren.se>
2023-04-26 18:46:56 +0200

    Hide 404 error on search results page

--- src/components/Pages/Search/index.tsx
+++ src/components/Pages/Search/index.tsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react'
 import { IApp } from 'utils/types'
 import Loading from 'components/Loading'
 import ErrorBoundaryEveryChild from 'components/ErrorBoundaryEveryChild'
 import useCallOnEscape from 'hooks/useCallOnEscape'
 import useSetBodyBackgroundColor from 'hooks/useSetBodyBackgroundColor'
 import useSearchHandler from 'hooks/useSearchHandler'
+import { StatusError } from 'utils/request'
 
 import Search from './Search'
 
@@ -17,13 +19,17 @@ export default function SearchPage({ app }: IProps) {
   useCallOnEscape(searchHandler.closeSearch)
   useSetBodyBackgroundColor('#475569')
 
+  const searchResultsError = searchHandler.searchResults.error
+
+  const error = useMemo(
+    () => (searchResultsError?.status === 404 ? null : searchResultsError),
+    [searchResultsError]
+  )
+
   return (
     <main role="main">
       <ErrorBoundaryEveryChild>
-        <Loading
-          loading={searchHandler.searchResults.loading}
-          error={searchHandler.searchResults.error}
-        />
+        <Loading loading={searchHandler.searchResults.loading} error={error} />
         <Search searchHandler={searchHandler} />
       </ErrorBoundaryEveryChild>
     </main>
