8377a23345e0f7729e73c2e7eec891d0fe9f5341
patrik.hultgren <info@patrikhultgren.se>
2023-04-26 08:26:16 +0200

    Remove unused import

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,30 +1,3 @@
-import {
-  ClearSkyDay,
-  ClearSkyNight,
-  ClearSkyPolarTwilight,
-  Cloudy,
-  FairDay,
-  FairNight,
-  FairPolarTwilight,
-  Fog,
-  HeavyRain,
-  HeavyRainAndThunder,
-  HeavyRainShowersDay,
-  HeavySnowShowersPolarTwilight,
-  HeavyRainShowersAndThunderDay,
-  HeavyRainShowersAndThunderNight,
-  HeavyRainShowersAndThunderPolarTwilight,
-  LightRain,
-  LightSleet,
-  LightSnow,
-  PartlyCloudyDay,
-  PartlyCloudyNight,
-  Rain,
-  RainShowersNight,
-  Sleet,
-  Snow,
-} from '@patrikhultgren/react-weather-icons'
-
 export const FORECAST_API_URL = process.env.REACT_APP_FORECAST_API_URL || ''
 
 export const ADDRESS_API_URL = process.env.REACT_APP_ADDRESS_API_URL || ''
