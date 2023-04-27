4e49b09feb6ec8faf338043e3ddabb59eb8e2427
patrik.hultgren <info@patrikhultgren.se>
2023-04-27 20:09:18 +0200

    Fix some validation errors

--- src/components/Pages/Search/Search/SearchForm.tsx
+++ src/components/Pages/Search/Search/SearchForm.tsx
@@ -41,7 +41,6 @@ export default function Search({ searchHandler }: IProps) {
       )}
       <button
         type="submit"
-        arial-label="Sök"
         className="bg-slate-100 py-2 px-4 hover:bg-slate-200"
       >
         <SearchIcon />
