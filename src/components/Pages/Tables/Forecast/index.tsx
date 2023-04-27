6a4cc92c1459f14ac5da2aef7fca401066430627
patrik.hultgren <info@patrikhultgren.se>
2023-04-27 17:37:25 +0200

    Skip load on scroll

--- src/components/Pages/Tables/Forecast/index.tsx
+++ src/components/Pages/Tables/Forecast/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import Container from 'components/Container'
 import ErrorBoundary from 'components/ErrorBoundary'
 import { IApp } from 'utils/types'
@@ -12,33 +11,14 @@ interface IProps {
 
 export default function Forecast({ app }: IProps) {
   const { days } = app
-  const [hasScrolled, setHasScrolled] = useState(false)
-
-  useEffect(() => {
-    if (hasScrolled) {
-      return
-    }
-
-    const onScroll = () => {
-      setHasScrolled(true)
-    }
-
-    window.addEventListener('scroll', onScroll, { passive: true })
-
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-    }
-  }, [hasScrolled])
 
   return days ? (
     <Container className={className}>
-      {days
-        .filter((_day, index) => hasScrolled || index < 5)
-        .map((day) => (
-          <ErrorBoundary key={day[0].time}>
-            <Day day={day} />
-          </ErrorBoundary>
-        ))}
+      {days.map((day) => (
+        <ErrorBoundary key={day[0].time}>
+          <Day day={day} />
+        </ErrorBoundary>
+      ))}
     </Container>
   ) : null
 }
