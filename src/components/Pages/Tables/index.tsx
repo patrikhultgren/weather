edfea2644e7fd78fd8d54647a48e5582e49d0009
patrik.hultgren <info@patrikhultgren.se>
2023-04-29 22:54:09 +0200

    Simplify structure

--- src/components/Pages/Tables/index.tsx
+++ src/components/Pages/Tables/index.tsx
@@ -1,15 +1,26 @@
 import { IApp } from 'utils/types'
 import PageLayoutForecast from 'components/PageLayout/Forecast'
-import Forecast from './Forecast'
+import Container from 'components/Container'
+import ErrorBoundary from 'components/Error/Boundary'
+import Day from './Day'
 
 interface IProps {
   app: IApp
 }
 
 export default function TablesPage({ app }: IProps) {
+  const { days } = app
   return (
     <PageLayoutForecast app={app} activeMenuItem="tables">
-      <Forecast app={app} />
+      {days && (
+        <Container className="mb-10">
+          {days.map((day) => (
+            <ErrorBoundary key={day[0].time}>
+              <Day day={day} />
+            </ErrorBoundary>
+          ))}
+        </Container>
+      )}
     </PageLayoutForecast>
   )
 }
