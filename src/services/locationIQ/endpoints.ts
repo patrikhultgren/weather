eefe39fbc8331f3dc3e0b48d9660aa12f0cbfdbf
patrik.hultgren <info@patrikhultgren.se>
2023-04-30 07:40:04 +0200

    Restructure app

--- src/services/locationIQ/endpoints.ts
+++ src/services/locationIQ/endpoints.ts
@@ -1,8 +1,8 @@
-import { YR_WEATHER_FORECAST_API_URL, LOQATION_IQ_API_KEY } from 'config'
+import { LOQATION_IQ_SEARCH_API_URL, LOQATION_IQ_API_KEY } from 'config'
 
 const endpoints = {
   searchUrl: (searchTerm: string): string =>
-    `${YR_WEATHER_FORECAST_API_URL}?key=${LOQATION_IQ_API_KEY}&q=${searchTerm.trim()}&format=json`,
+    `${LOQATION_IQ_SEARCH_API_URL}?key=${LOQATION_IQ_API_KEY}&q=${searchTerm.trim()}&format=json`,
 }
 
 export default endpoints
