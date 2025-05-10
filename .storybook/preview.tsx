66ffc04588bf19750c318f4b483196870b6ad93b
patrikhultgren <info@patrikhultgren.se>
2025-05-10 19:55:42 +0200

    Install Storybook 8 (#2)
    Install Storybook 8 and add old stories again

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,24 @@
 import '../src/css/style.css'
 import type { Preview } from '@storybook/react'
+import { MemoryRouter } from 'react-router-dom'
+import React from 'react'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/i,
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 }
 
 export default preview
---
+++ src/stories/Search.stories.ts
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import searchHandlerWaitingForInput from '__fixtures__/searchHandler/history'
+import searchHandlerSearchResults from '__fixtures__/searchHandler/searchResults'
+import searchHandlerNoSearchResults from '__fixtures__/searchHandler/noSearchResults'
+import waitingForInputSearchResults from '__fixtures__/searchHandler/waitingForInput'
+import loadingSearchResults from '__fixtures__/searchHandler/loadingSearchResults'
+import loadingWithSearchResults from '__fixtures__/searchHandler/loadingWithSearchResults'
+import SearchHandler from 'pages/Search/Handler'
+
+const meta = {
+  title: 'Pages/Search',
+  component: SearchHandler,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof SearchHandler>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const WaitingForInput: Story = {
+  args: {
+    searchHandler: waitingForInputSearchResults,
+  },
+}
+
+export const LoadingSearchResults: Story = {
+  args: {
+    searchHandler: loadingSearchResults,
+  },
+}
+
+export const LoadingWithSearchResults: Story = {
+  args: {
+    searchHandler: loadingWithSearchResults,
+  },
+}
+
+export const History: Story = {
+  args: {
+    searchHandler: searchHandlerWaitingForInput,
+  },
+}
+
+export const SearchResults: Story = {
+  args: {
+    searchHandler: searchHandlerSearchResults,
+  },
+}
+
+export const NoSearchResults: Story = {
+  args: {
+    searchHandler: searchHandlerNoSearchResults,
+  },
+}
