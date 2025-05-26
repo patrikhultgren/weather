3342639fde7c1858796ddac7e2222579f12d6fbf
patrik.hultgren <info@patrikhultgren.se>
2025-05-26 21:02:27 +0200

    Use language when getting address

--- src/hooks/app/useAddress.ts
+++ src/hooks/app/useAddress.ts
@@ -4,6 +4,7 @@ import useFetch from 'hooks/useFetch'
 import { addPosition } from 'utils/position'
 import { IPosition, IAddress } from 'utils/types'
 import endpoints from 'services/bigDataCloud/endpoints'
+import { useTranslation } from 'context/TranslationProvider'
 
 interface IProps {
   position: IPosition
@@ -11,14 +12,17 @@ interface IProps {
 }
 
 const useAddress = ({ position, setPositions }: IProps): IQuery<IAddress> => {
+  const { language } = useTranslation()
+
   const run = useMemo(
     () => Boolean(position.latitude && position.longitude && !position.city),
     [position]
   )
 
   const url = useMemo(
-    () => endpoints.getAddressUrl(position.latitude, position.longitude),
-    [position.latitude, position.longitude]
+    () =>
+      endpoints.getAddressUrl(position.latitude, position.longitude, language),
+    [position.latitude, position.longitude, language]
   )
 
   const address = useFetch<IAddress>({ url, run })
